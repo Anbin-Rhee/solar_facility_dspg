3f091983a25e4e112f44171fd5d1b7fb71837651
Aziza Altyyeva <altyyevaa@berea.edu>
2025-06-24 15:16:34 -0400

    Updated merge code

--- merge_code.R
+++ merge_code.R
@@ -3,10 +3,10 @@ library(dplyr)
 library(stringr)
 library(readr)
 library(lubridate)
+#install.packages("stargazer")
+library(stargazer)
 
-
-
-
+#Creating the 2010- 2023 Grid
 va_counties <- get_acs(
   geography = "county",
   variables = "B01003_001",  # total population
@@ -35,15 +35,18 @@ county_year_grid <- expand.grid(
 View(county_year_grid)
 
 
-
+# Adding Land Transactions to the grid
 land_price_summary <- read_csv("clean_data/LandPrice.csv")
+colnames(land_price_summary)
 
 
-land_price_summary <- land_price_summary %>%
-  group_by(County, Year) %>%
-  summarize(LandPrice = mean(LandPrice, na.rm = TRUE), .groups = "drop")
- 
+merged_data <- county_year_grid
+merged_data <- merged_data %>%
+  left_join(land_price_summary %>% select(County, Year, Price_Per_Acre),
+            by = c("County", "Year"))
+View(merged_data)
 
+#Merging Corn Yield to the grid
 #_____________________________________________________________________________
 new_corn_data <- read_csv("clean_data/new_corn_data.csv")
 new_corn_data <- new_corn_data %>%
@@ -56,6 +59,7 @@ merged_data <- merged_data %>%
 View(merged_data)
 
 
+#Merging SOy Yield data to the grid
 #_______________________________________________________________________________
 new_soy_data <- read_csv("clean_data/new_soy_data.csv")
 new_soy_data <- new_soy_data %>%
@@ -67,6 +71,8 @@ summary(merged_data$SoyYield)
 
 View(merged_data)
 
+
+#Merging house age to the grid
 #_______________________________________________________________________________
 housing_age_all <- read_csv("clean_data/va_avg_housing_age_all_years.csv")
 colnames(housing_age_all)
@@ -78,6 +84,7 @@ merged_data <- merged_data %>%
 View(merged_data)
 
 
+#Merging population data to the grid
 #________________________________________________________________________________
 va_pop <- read_csv("clean_data/va_population.csv")
 colnames(va_pop)
@@ -88,6 +95,8 @@ merged_data <- merged_data %>%
             by = c("County", "Year"))
 View(merged_data)
 
+
+#Merging the hosuing status data to the grid
 #________________________________________________________________________________
 
 housing_status <- read_csv("clean_data/housing_status.csv")
@@ -107,7 +116,6 @@ View(merged_data)
 #________________________________________________________________________________
 solar <- read_csv("clean_data/ solar_fac.csv")
 
-
 treated_counties <- solar %>%
   mutate(
     County = str_to_title(County),
@@ -115,7 +123,7 @@ treated_counties <- solar %>%
     County = str_trim(County)
   ) %>%
   group_by(County) %>%
-  summarize(SF_Year = min(Year, na.rm = TRUE)) %>%
+  summarize(SF_Year = min(Year, na.rm = TRUE)) %>% #the first year the county had solar facility
   ungroup()
 
 merged_data <- merged_data %>%
@@ -142,18 +150,47 @@ View(merged_data)
 #   head(10)
 
 
-
 View(merged_data)
 
 
-model <- lm(LandPrice ~ Treated + Post + DiD, data = merged_data)
-summary(model)
 
+cleaned_model_data <- merged_data %>%
+  filter(!is.na(Price_Per_Acre),
+         Price_Per_Acre > 0,
+         !is.na(DiD),
+         !is.na(Population),
+         Population > 0,
+         !is.na(CornYield),
+         CornYield > 0,
+         !is.na(SoyYield),
+         SoyYield > 0,
+         !is.na(HousingAge),
+         HousingAge > 0,
+         !is.na(TotalHousingUnits),
+         TotalHousingUnits > 0)
 
-model <- lm(LandPrice ~ DiD + Population + CornYield + SoyYield + HousingAge + TotalHousingUnits, data = merged_data)
-summary(model)
+model1 <- lm(log(Price_Per_Acre) ~ DiD + log(Population) + log(CornYield) + log(SoyYield) + log(HousingAge) + log(TotalHousingUnits ) + factor(County) + factor(Year),
+            data = cleaned_model_data)
+summary(model1) 
 
+model2 <- lm(log(Price_Per_Acre) ~ DiD + log(Population) + log(CornYield) + log(SoyYield) + log(HousingAge) + log(TotalHousingUnits ),
+             data = cleaned_model_data)
+summary(model2)
 
-#write.csv(broom::tidy(model), "model_results.csv", row.names = FALSE)
+model3 <- lm(log(Price_Per_Acre) ~ DiD + factor(County) + factor(Year), data = cleaned_model_data)
+#summary(model)
 
+stargazer(model1, model2, model3,
+          type = 'text',
+          out = "regression_output.txt",
+          omit = c("County", "Year"), p.auto = TRUE)
 
+colnames(cleaned_model_data)
+model_vacancy <- lm(log(VacantUnits) ~ DiD + factor(County) + factor(Year), data = cleaned_model_data)
+summary(model_vacancy)
+# model_results <- read_csv("model_results.csv")
+
+model_vacancy_2 <- lm(log(VacantUnits) ~ DiD, data = cleaned_model_data)
+stargazer(model_vacancy, model_vacancy_2, type = "text", out = "vacancy_model_output.txt",
+          omit = c("County", "Year"), p.auto = TRUE)
+# View(model_results)
